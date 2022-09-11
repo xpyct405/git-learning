bfa2a727a4db76904b7746ac48770f3606ee7a9c
Rustam Kuzyaev <90527704+xpyct405@users.noreply.github.com>
2022-09-11 09:58:05 +0300

    feat: add name for DOM

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-document.body.innerHTML = '<h1>Hello from JavaScript</h1>'
+let header = document.body.innerHTML = '<h1>Hello from JavaScript</h1>'
 
-console.log(document.body.innerHTML);
+console.log(header);
